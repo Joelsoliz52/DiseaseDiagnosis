7f89f72a4980696dff4019c3292d877279a231bc
Joelsoliz52 <joelsoliz4321@gmail.com>
2020-10-28 08:41:25 -0400

    Inicio

---
+++ app/src/main/java/com/wow/diseasediagnosis/ui/activity/SystemsActivity.java
@@ -0,0 +1,16 @@
+package com.wow.diseasediagnosis.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wow.diseasediagnosis.R;
+
+public class SystemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_systems);
+    }
+}
